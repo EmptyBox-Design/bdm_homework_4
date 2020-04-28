76641bd54c1be5f7de27d9373c8c913889a955c5
BRANDON-RIG\Brandon Pachuca <bpachuca@kpf.com>
2020-04-27 21:39:23 -0400

    Update read_data.py

--- read_data.py
+++ read_data.py
@@ -5,18 +5,17 @@ def readData(f):
   with gzip.open(f, mode='rt') as recrods:
     reader = csv.reader(recrods)
 
-
+    i = 0
     for row in reader:
       print(row)
-      break
+      i += 1
+      if(i == 2):
+        break
 
 if __name__ == "__main__":
 
   import sys
 
   file_location = sys.argv[1]
-  print(file_location)
 
   readData(file_location)
-
-tpep_pickup_datetime,tpep_dropoff_datetime,pickup_latitude,pickup_longitude,dropoff_latitude,dropoff_longitude
